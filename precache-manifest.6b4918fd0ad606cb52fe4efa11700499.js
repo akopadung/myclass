ad134c006a267f2002fcb599e0940c593520b6a7
phukhieo <akopadung>
2020-04-14 16:24:10 +0700

    Updates

---
+++ precache-manifest.6b4918fd0ad606cb52fe4efa11700499.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b40f231e520518d7032df27f2fdc19",
+    "url": "/myclass/index.html"
+  },
+  {
+    "revision": "7f848e0539aa962abcb7",
+    "url": "/myclass/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aabecc88c1d48f4ebf93",
+    "url": "/myclass/static/js/2.736ec852.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myclass/static/js/2.736ec852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f848e0539aa962abcb7",
+    "url": "/myclass/static/js/main.c7210eb6.chunk.js"
+  },
+  {
+    "revision": "0be151403d9f6b2353bb",
+    "url": "/myclass/static/js/runtime-main.53ff983f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myclass/static/media/logo.5d5d9eef.svg"
+  }
+]);
